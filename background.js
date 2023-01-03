017681e7e239c4a7b4fcb5b124a0aeec4c22a87e
IMasterline <101966601+IMasterline@users.noreply.github.com>
2023-01-03 14:38:00 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
-chrome.storage.local.get("score", function (isntsafe) {
-	console.log(isntsafe.score)
-	if (isntsafe.score == 10)
-		chrome.action.setPopup({ popup: "test.html"})
+var score = 5
+var isblacklisted = true
+
+chrome.tabs.onActivated.addListener(function (changepopup) {
+	if (score == 10){
+		chrome.action.setPopup({ popup: "popups/safe.html"})
+	}	
+	if (score <= 9 && score >= 6){
+		chrome.action.setPopup({ popup: "popups/risky.html"})
+	}
+	if (score <= 5 && score >= 2){
+		chrome.action.setPopup({ popup: "popups/unsafe.html"})
+	}
+	if (score == 1){
+		chrome.action.setPopup({ popup: "popups/scam.html"})
+	}
+	if (isblacklisted == true){
+		chrome.action.setPopup({ popup: "popups/blacklisted.html"})
+	}
 });
