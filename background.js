f217605bd78c20f6e35666426ad95eeac92cf8d8
IMasterline <101966601+IMasterline@users.noreply.github.com>
2023-01-09 15:12:52 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
+//variables
 var score = 5
 var isblacklisted = true
 
+//on tab change: set correct popup
 chrome.tabs.onActivated.addListener(function (changepopup) {
 	if (score == 10){
 		chrome.action.setPopup({ popup: "popups/safe.html"})
