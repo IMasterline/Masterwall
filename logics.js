810b47bc8161aa6436b387512faa1a02def9e8b5
IMasterline <101966601+IMasterline@users.noreply.github.com>
2023-02-01 11:55:53 +0200

    Update logics.js

--- logics.js
+++ logics.js
@@ -1,6 +1,20 @@
-function reportWebsite(){
+ var  bgp = chrome.extension.getBackgroundPage()
+ document.addEventListener('DOMContentLoaded', function () {
+    
+    var btblacklist = document.getElementById("btblacklist");
+    btblacklist.addEventListener('click', blacklistWebsite);
+});
+
+
+function blacklistWebsite(){
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    // use `url` here inside the callback because it's asynchronous!
+	
 	var xhr = new XMLHttpRequest();
-	xhr.open('post','http://localhost:8080/addToBlacklist/' + tab.url, true);
+	var pathArray = tabs[0].url.split( '/' );
+	var host = pathArray[2];
+	xhr.open('post','http://localhost:8080/addToBlacklist/' + host, true);
 	xhr.send();
+	
+	});
 }
-reportWebsite();
