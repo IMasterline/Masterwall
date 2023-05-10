036416dac71d5c8dd39b1acc031a4f1eb9817269
IMasterline <101966601+IMasterline@users.noreply.github.com>
2023-05-10 13:39:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import json
 import sqlite3
 import random
+import requests
 
 """
 #connect to db
@@ -22,13 +23,6 @@ app = Flask(__name__)
 
 isblacklisted = 0
 reportscount = 0
-spamcooldown = 0
-
-async def reducecooldown():
-    global spamcooldown
-    while (spamcooldown > 0):
-        spamcooldown = spamcooldown - 1
-
 
 
 @app.route("/")
@@ -50,6 +44,7 @@ def insertToBlacklist(url):
         print("already in table")
     else:
         cur.execute("INSERT INTO blacklist (URL) VALUES (?)", (url, ))
+        print("website blacklisted")
 
     #save and close connection
     con.commit()
@@ -61,30 +56,27 @@ def insertToBlacklist(url):
 
 @app.route("/addToReported/<url>", methods=["post"])
 def insertToReported(url):
-    global spamcooldown
-    if (spamcooldown == 0):
-        #connect to db
-        con = sqlite3.connect("db.db")
-        #create cursor
-        cur = con.cursor()
-        #commands section
-        print(url)
-        cur.execute("SELECT * FROM reported WHERE URL='" + url + "'")
-        if len(cur.fetchall()) > 0:
-            print(cur.fetchall())
-            cur.execute("UPDATE reported SET reports = reports + 1 WHERE URL='" + url + "'")
-            print("reports += 1")
-            spamcooldown = 60
-        else:
-            cur.execute("INSERT INTO reported (URL,reports) VALUES (?,?)", (url,1))
-        
-        #save and close connection
+    # get the public IP address of the client
+    ip = requests.get('https://api.ipify.org').text
+    # connect to db
+    con = sqlite3.connect("db.db")
+    # create cursor
+    cur = con.cursor()
+    # commands section
+    cur.execute("SELECT * FROM reported WHERE URL=? AND IP=?", (url, ip))
+    if len(cur.fetchall()) > 0:
+        # user has already reported this URL
+        con.close()
+        print("This IP range has already reported this URL.")
+        return jsonify({"error": "You have already reported this URL."})
+    else:
+        cur.execute("INSERT INTO reported (URL, IP) VALUES (?,?)", (url, ip))
+        print("URL has been reported")
+        # save and close connection
         con.commit()
         con.close()
-    else:
-        print("on cooldown")
-    
-    return jsonify({"code": "200"})
+        return jsonify({"code": "200"})
+
     
     
 @app.route("/checkBlacklist/<url>", methods=["post"])
@@ -130,14 +122,14 @@ def checkReportedDB():
         # Create cursor
         cur = con.cursor()
         # Execute SQL command
-        cur.execute("SELECT reports FROM reported WHERE URL=?", (hostname,))
-        result = cur.fetchone()
+        cur.execute("SELECT COUNT(*) FROM reported WHERE URL=?", (hostname,))
+        result = cur.fetchone()[0]
         # Close connection
         con.close()
 
-        if result is not None:
+        if result > 0:
             # The website has been reported
-            reportscount = result[0]
+            reportscount = result
             print("Website has been reported")
             return jsonify({"reportscount": reportscount})
         else:
