3d36d1373b0da5b34beaced2985f608a8f3d7172
IMasterline <101966601+IMasterline@users.noreply.github.com>
2023-06-19 17:41:42 +0300

    Update logics.js

--- popups/logics.js
+++ popups/logics.js
@@ -1,37 +1,34 @@
- var bgp = chrome.extension.getBackgroundPage()
- document.addEventListener('DOMContentLoaded', function () {
-    
-    var btblacklist = document.getElementById("btblacklist");
-    btblacklist.addEventListener('click', blacklistWebsite);
-	
-	var btreport = document.getElementById("btreport");
-    btreport.addEventListener('click', reportWebsite);
-});
+class Popup {
+  constructor() {
+    document.addEventListener('DOMContentLoaded', () => {
+      const btblacklist = document.getElementById("btblacklist");
+      btblacklist.addEventListener('click', this.blacklistWebsite);
 
+      const btreport = document.getElementById("btreport");
+      btreport.addEventListener('click', this.reportWebsite);
+    });
+  }
 
-function blacklistWebsite(){
-	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    // use `url` here inside the callback because it's asynchronous!
-	
-	var xhr = new XMLHttpRequest();
-	var pathArray = tabs[0].url.split( '/' );
-	var host = pathArray[2];
-	xhr.open('post','http://localhost:8080/addToBlacklist/' + host, true);
-	xhr.send();
-	
-	});
+  blacklistWebsite() {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+      const xhr = new XMLHttpRequest();
+      const pathArray = tabs[0].url.split('/');
+      const host = pathArray[2];
+      xhr.open('POST', 'http://localhost:8080/addToBlacklist/' + host, true);
+      xhr.send();
+    });
+  }
+
+  reportWebsite() {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+      const xhr = new XMLHttpRequest();
+      const pathArray = tabs[0].url.split('/');
+      const host = pathArray[2];
+      xhr.open('POST', 'http://localhost:8080/addToReported/' + host, true);
+      xhr.send();
+    });
+  }
 }
 
+const popup = new Popup();
 
-function reportWebsite(){
-	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    // use `url` here inside the callback because it's asynchronous!
-	
-	var xhr = new XMLHttpRequest();
-	var pathArray = tabs[0].url.split( '/' );
-	var host = pathArray[2];
-	xhr.open('post','http://localhost:8080/addToReported/' + host, true);
-	xhr.send();
-	
-	});
-}
